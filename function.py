4e8adcb47b086630ee78d1a1dc9d1adf6a26a393
ygh1254 <ygh1254@hanyang.ac.kr>
2025-02-04 22:34:59 +0900

    feat: move AuthToken to config.py

--- function.py
+++ function.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import math
-from config import APP_KEY, APP_SECRET, STOCK_LIST
+from config import AUTH_TOKEN, APP_KEY, APP_SECRET, STOCK_LIST
 
 # 리스트 사전 정의
 Stock_list = STOCK_LIST
@@ -14,36 +14,36 @@ Actual_sell_price = {stock: 0 for stock in Stock_list}
 Quantity = {stock: 0 for stock in Stock_list}
 Log = []
 
-def AuthToken():
-    url = "https://openapivts.koreainvestment.com:29443/oauth2/tokenP"
+# def AuthToken():
+#     url = "https://openapivts.koreainvestment.com:29443/oauth2/tokenP"
 
-    payload = json.dumps({
-    "grant_type": "client_credentials",
-    "appkey": APP_KEY,
-    "appsecret": APP_SECRET
-    })
+#     payload = json.dumps({
+#     "grant_type": "client_credentials",
+#     "appkey": APP_KEY,
+#     "appsecret": APP_SECRET
+#     })
     
-    headers = {
-    'content-type': 'application/json'
-    }
+#     headers = {
+#     'content-type': 'application/json'
+#     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+#     response = requests.request("POST", url, headers=headers, data=payload)
     
-    '''
-    print(response.text)
-    {"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjBkODA0NjIxLTc5MTQtNDgyZS04Njg2LTQzMjhkNDcwZDYxOSIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTczODczMDM0MCwiaWF0IjoxNzM4NjQzOTQwLCJqdGkiOiJQU3ptVWFIa0c2WWphVUxSUGx0NGd4dVpva0h5SXVUeXRvZkQifQ.d4kXToN2VfayMB0hnn9VKbHURYFWPTVN-TjK-6uDc--PqdvX6yeomJ9QYLTqpHz-rf_Hh9vkBbWPdWPZUceq0g",
-    "access_token_token_expired":"2025-02-05 13:39:00",
-    "token_type":"Bearer",
-    "expires_in":86400}
-    '''
+#     '''
+#     print(response.text)
+#     {"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImF1ZCI6IjBkODA0NjIxLTc5MTQtNDgyZS04Njg2LTQzMjhkNDcwZDYxOSIsInByZHRfY2QiOiIiLCJpc3MiOiJ1bm9ndyIsImV4cCI6MTczODczMDM0MCwiaWF0IjoxNzM4NjQzOTQwLCJqdGkiOiJQU3ptVWFIa0c2WWphVUxSUGx0NGd4dVpva0h5SXVUeXRvZkQifQ.d4kXToN2VfayMB0hnn9VKbHURYFWPTVN-TjK-6uDc--PqdvX6yeomJ9QYLTqpHz-rf_Hh9vkBbWPdWPZUceq0g",
+#     "access_token_token_expired":"2025-02-05 13:39:00",
+#     "token_type":"Bearer",
+#     "expires_in":86400}
+#     '''
     
-    token = f"{json.loads(response.text)['token_type']} {json.loads(response.text)['access_token']}"
+#     token = f"{json.loads(response.text)['token_type']} {json.loads(response.text)['access_token']}"
     
-    return token
+#     return token
 
-# 추후 자동화하여 1일 1회 발급 받는 방향으로 수정
-# 1분에 1회 발급 가능
-AUTH_TOKEN = AuthToken()
+# # 추후 자동화하여 1일 1회 발급 받는 방향으로 수정
+# # 1분에 1회 발급 가능
+# AUTH_TOKEN = AuthToken()
 
 # Header 양식 출력
 def Headers(tr_id):

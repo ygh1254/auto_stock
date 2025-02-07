a53fc0d14e2a123d0b938e319bca373436811de3
ygh1254 <ygh1254@hanyang.ac.kr>
2025-02-07 17:14:21 +0900

    feat: connect functions

--- function.py
+++ function.py
@@ -8,10 +8,27 @@ from config import AUTH_TOKEN, APP_KEY, APP_SECRET, STOCK_LIST
 Stock_list = STOCK_LIST
 Open_price = {stock: 0 for stock in Stock_list}
 Target_buy_price = {stock: 0 for stock in Stock_list}
-Actual_buy_price = {stock: 0 for stock in Stock_list}
 Target_sell_price = {stock: 0 for stock in Stock_list}
-Actual_sell_price = {stock: 0 for stock in Stock_list}
-Quantity = {stock: 0 for stock in Stock_list}
+Current_stock = {stock: {'pdno': {stock}, # 종목 번호
+                        'prdt_name': {stock}, # 종목 이름
+                        'hldg_qty': '0', # 보유 수량
+                        'ord_psbl_qty': '0', # 주문 가능 수량
+                        'pchs_avg_pric': '0', # 매입 평균 가격
+                        'prpr': '0', # 현재가
+                        'evlu_amt': '0', # 평가금액
+                        'evlu_pfls_amt': '0', # 수익
+                        'evlu_pfls_rt': '0' # 수익률
+                        } for stock in Stock_list}
+Current_account = {'dnca_tot_amt': '', # 예수금 총 금액
+                    'nxdy_excc_amt': '', # 익일 정산 금액
+                    'prvs_rcdl_excc_amt': '', # 가수도 정산 금액
+                    'thdt_buy_amt': '', # 금일 매수 금액
+                    'thdt_sll_amt': '', # 금일 매도 금액
+                    'tot_evlu_amt': '', # 총 평가 금액
+                    'nass_amt': '', # 순 자산 금액
+                    'asst_icdc_amt': '', # 자산 증감액
+                    'asst_icdc_erng_rt': '', # 자산 증감 수익률
+                    }
 Log = []
 
 # Header 양식 출력
@@ -56,9 +73,9 @@ def RoundNumber(number):
        return 1000 * math.floor(number / 1000)
 
 # 시가 받아오기 - 09시 1회 실행
-def OpenPrice(Stock_list, Open_price, Target_buy_price):
+def OpenPrice(Stock_list, Open_price, Target_buy_price, Current_stock):
     for stock in Stock_list:
-        print(f'Now loading stock {stock}')
+        print(f'{Current_stock[stock]['prdt_name']}  정보를 가져옵니다')
         url = f"https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-price?fid_cond_mrkt_div_code=J&fid_input_iscd={stock}"
         payload = ""
         headers = Headers('FHKST01010100')
@@ -98,10 +115,8 @@ def OpenPrice(Stock_list, Open_price, Target_buy_price):
         '''
         
         open_price = int(json.loads(response.text)['output']['stck_oprc'])
-        # target_buy_price = RoundNumber(open_price * 0.95)
+        target_buy_price = RoundNumber(open_price * 0.95)
         
-        # Test Code
-        target_buy_price = RoundNumber(open_price * 0.99)
         Open_price[stock] = open_price
         Target_buy_price[stock] = target_buy_price
 
@@ -111,7 +126,7 @@ def OpenPrice(Stock_list, Open_price, Target_buy_price):
     return Open_price, Target_buy_price
 
 # 현재가 받아오기
-def LivePrice(Stock_list, Target_buy_price, Actual_buy_price, Target_sell_price, Actual_sell_price):
+def LivePrice(Stock_list, Target_buy_price, Target_sell_price, Current_stock):
     for stock in Stock_list:
         url = f"https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/quotations/inquire-price?fid_cond_mrkt_div_code=J&fid_input_iscd={stock}"
     
@@ -122,36 +137,39 @@ def LivePrice(Stock_list, Target_buy_price, Actual_buy_price, Target_sell_price,
         response = requests.request("GET", url, headers=headers, data=payload)
         CatchError(response)
         
-        print(response.text)
-        
         live_price = int(json.loads(response.text)['output']['stck_prpr'])
         target_buy_price = Target_buy_price[stock]
         target_sell_price = Target_sell_price[stock]
         
-        print(live_price, target_buy_price)
+        print(f"{Current_stock[stock]['prdt_name']} 목표구매가격 {target_buy_price}원, 시가 {live_price}원")
+        print(f"{Current_stock[stock]['prdt_name']} 목표판매가격 {target_sell_price}원, 시가 {live_price}원")
+        print("-----------------------------------------------------------")
         
-        # 현재가가 목표 매수 가격 이하인 경우 & 주식 수량이 0인 경우
-        # 현재 계좌 잔고 확인하는 기능 추가할 것
-        if (live_price <= target_buy_price) & (Quantity[stock] == 0):
-        # if (live_price <= target_buy_price):
-            time.sleep(0.5)
-            actual_buy_price = BuyStock(stock, target_buy_price)
-            
-            Actual_buy_price[stock] = actual_buy_price
-            Target_sell_price[stock] = RoundNumber(actual_buy_price * 1.03)
+        # Current_stock[stock]['ord_psbl_qty']가 0인 경우 구매 조건식 확인
+        if Current_stock[stock]['ord_psbl_qty'] == 0:
+            # 시가가 목표 매수가 이하
+            if (live_price <= target_buy_price):
+                time.sleep(0.5)
+                BuyStock(stock, target_buy_price, Log)
+                CheckStock()
+                
+                Target_sell_price[stock] = RoundNumber(Current_stock[stock]['pchs_avg_pric'] * 1.03)
         
-        # 현재가가 목표 매도 가격 이상인 경우 & 주식 수량이 0이 아닌 경우
-        # 현재 주식 잔고 확인하는 기능 추가할 것 -> 추가 시 Quantity 없어도 될 것 같음
-        elif (live_price >= target_sell_price) & (Quantity[stock] != 0):
-            time.sleep(0.5)
-            actual_sell_price = SellStock(stock, target_sell_price)
-
-            Actual_sell_price[stock] = actual_sell_price
+        # Current_stock[stock]['ord_psbl_qty']가 0이 아닌 경우 판매 조건식 확인
+        else :
+            # target_sell_price가 0인 경우 초기화 값이므로 실행하지 않음
+            if (target_sell_price != 0) & (live_price >= target_sell_price):
+                time.sleep(0.5)
+                SellStock(stock, target_sell_price, Log)
+                CheckStock()
         
         time.sleep(0.5)
+        
+    print(Log)
+    print(Current_account)
 
 # 매수
-def BuyStock(stock, target_buy_price):
+def BuyStock(stock, target_buy_price, Log):
     
     if CheckBuyStock(stock, target_buy_price):
         
@@ -192,17 +210,12 @@ def BuyStock(stock, target_buy_price):
             }
         }
         '''
+        Log.append(f"{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime)} | 주식 {stock} | {target_buy_price} 구매")
         
-        #실제 매수가
-        actual_buy_price = int(target_buy_price)
-        
-        print(f"Bought {stock}")
-        Quantity[stock] += 1
-        
-        return actual_buy_price
+        return print(f"{stock}을 구매합니다"), Log
     
 # 매도
-def SellStock(stock, target_sell_price):
+def SellStock(stock, target_sell_price, Log):
         
     url = "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/trading/order-cash"
 
@@ -220,13 +233,12 @@ def SellStock(stock, target_sell_price):
     response = requests.request("POST", url, headers=headers, data=payload)
     CatchError(response)
     
-    # 실제 매도가
-    actual_sell_price = int(target_sell_price)
+    Log.append(f"{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime)} | 주식 {stock} | {target_sell_price} 판매")
     
-    return actual_sell_price
+    return print(f"{stock}을 판매합니다"), Log
     
 # 주식 잔고 조회
-def CheckStock(stock):
+def CheckStock():
     url = "https://openapivts.koreainvestment.com:29443/uapi/domestic-stock/v1/trading/inquire-balance?CANO=50124241&ACNT_PRDT_CD=01&AFHR_FLPR_YN=N&OFL_YN=&INQR_DVSN=01&UNPR_DVSN=01&FUND_STTL_ICLD_YN=N&FNCG_AMT_AUTO_RDPT_YN=N&PRCS_DVSN=00&CTX_AREA_FK100=&CTX_AREA_NK100="
 
     payload = ""
@@ -234,77 +246,47 @@ def CheckStock(stock):
 
     response = requests.request("GET", url, headers=headers, data=payload)
     CatchError(response)
-    '''
-    {
-    "ctx_area_fk100": "                                                                                                    ",
-    "ctx_area_nk100": "                                                                                                    ",
-    "output1": [
-        {
-            "pdno": "005930",
-            "prdt_name": "삼성전자",
-            "trad_dvsn_name": "현금",
-            "bfdy_buy_qty": "0",
-            "bfdy_sll_qty": "0",
-            "thdt_buyqty": "1",
-            "thdt_sll_qty": "0",
-            "hldg_qty": "1",
-            "ord_psbl_qty": "1",
-            "pchs_avg_pric": "51000.0000",
-            "pchs_amt": "51000",
-            "prpr": "51000",
-            "evlu_amt": "51000",
-            "evlu_pfls_amt": "0",
-            "evlu_pfls_rt": "0.00",
-            "evlu_erng_rt": "0.00000000",
-            "loan_dt": "",
-            "loan_amt": "0",
-            "stln_slng_chgs": "0",
-            "expd_dt": "",
-            "fltt_rt": "-2.67000000",
-            "bfdy_cprs_icdc": "1400",
-            "item_mgna_rt_name": "20%",
-            "grta_rt_name": "",
-            "sbst_pric": "0",
-            "stck_loan_unpr": "0.0000"
-        }
-    ],
-    "output2": [
-        {
-            "dnca_tot_amt": "10000000",
-            "nxdy_excc_amt": "10000000",
-            "prvs_rcdl_excc_amt": "9949000",
-            "cma_evlu_amt": "0",
-            "bfdy_buy_amt": "0",
-            "thdt_buy_amt": "51000",
-            "nxdy_auto_rdpt_amt": "0",
-            "bfdy_sll_amt": "0",
-            "thdt_sll_amt": "0",
-            "d2_auto_rdpt_amt": "0",
-            "bfdy_tlex_amt": "0",
-            "thdt_tlex_amt": "0",
-            "tot_loan_amt": "0",
-            "scts_evlu_amt": "51000",
-            "tot_evlu_amt": "10000000",
-            "nass_amt": "10000000",
-            "fncg_gld_auto_rdpt_yn": "",
-            "pchs_amt_smtl_amt": "51000",
-            "evlu_amt_smtl_amt": "51000",
-            "evlu_pfls_smtl_amt": "0",
-            "tot_stln_slng_chgs": "0",
-            "bfdy_tot_asst_evlu_amt": "10000000",
-            "asst_icdc_amt": "0",
-            "asst_icdc_erng_rt": "0.00000000"
-        }
-    ],
-    "rt_cd": "0",
-    "msg_cd": "20310000",
-    "msg1": "모의투자 조회가 완료되었습니다.                                                 "
-    }
-    '''
-    # pdno와 stock 비교 후 판매
-    # 테스트 단계에서는 주식 수량이 1로 고정되어 있으나 추후 수량이 늘어나면 수정할 필요 있음.
     
-    return True
+    current_stock = json.loads(response.text)['output1']
+
+    # {'pdno': '005930', 'prdt_name': '삼성전자', 'trad_dvsn_name': '현금', 'bfdy_buy_qty': '0', 'bfdy_sll_qty': '0', 'thdt_buyqty': '0', 'thdt_sll_qty': '0', 'hldg_qty': '1', 'ord_psbl_qty': '1', 'pchs_avg_pric': '51000.0000', 'pchs_amt': '51000', 'prpr': '53800', 'evlu_amt': '53800', 'evlu_pfls_amt': '2800', 'evlu_pfls_rt': '5.49', 'evlu_erng_rt': '5.49019608', 'loan_dt': '', 'loan_amt': '0', 'stln_slng_chgs': '0', 'expd_dt': '', 'fltt_rt': '-0.37000000', 'bfdy_cprs_icdc': '200', 'item_mgna_rt_name': '20%', 'grta_rt_name': '', 'sbst_pric': '0', 'stck_loan_unpr': '0.0000'}
+    # {'pdno': '010955', 'prdt_name': 'S-Oil우', 'trad_dvsn_name': '현금', 'bfdy_buy_qty': '7', 'bfdy_sll_qty': '0', 'thdt_buyqty': '0', 'thdt_sll_qty': '0', 'hldg_qty': '7', 'ord_psbl_qty': '7', 'pchs_avg_pric': '39550.0000', 'pchs_amt': '276850', 'prpr': '39100', 'evlu_amt': '273700', 'evlu_pfls_amt': '-3150', 'evlu_pfls_rt': '-1.14', 'evlu_erng_rt': '-1.13780025', 'loan_dt': '', 'loan_amt': '0', 'stln_slng_chgs': '0', 'expd_dt': '', 'fltt_rt': '-1.14000000', 'bfdy_cprs_icdc': '450', 'item_mgna_rt_name': '50%', 'grta_rt_name': '', 'sbst_pric': '0', 'stck_loan_unpr': '0.0000'}
+
+    current_account = json.loads(response.text)['output2']
+
+    # [{'dnca_tot_amt': '9949000', 'nxdy_excc_amt': '9672120', 'prvs_rcdl_excc_amt': '9672120', 'cma_evlu_amt': '0', 'bfdy_buy_amt': '276850', 'thdt_buy_amt': '0', 'nxdy_auto_rdpt_amt': '0', 'bfdy_sll_amt': '0', 'thdt_sll_amt': '0', 'd2_auto_rdpt_amt': '0', 'bfdy_tlex_amt': '30', 'thdt_tlex_amt': '0', 'tot_loan_amt': '0', 'scts_evlu_amt': '327500', 'tot_evlu_amt': '9999620', 'nass_amt': '9999620', 'fncg_gld_auto_rdpt_yn': '', 'pchs_amt_smtl_amt': '327850', 'evlu_amt_smtl_amt': '327500', 'evlu_pfls_smtl_amt': '-350', 'tot_stln_slng_chgs': '0', 'bfdy_tot_asst_evlu_amt': '10002970', 'asst_icdc_amt': '-3350', 'asst_icdc_erng_rt': '-0.03349005'}]
+    
+    for item in current_stock:
+        Current_stock[item['pdno']] = {'pdno': item['pdno'], # 종목 번호
+                                 'prdt_name': item['prdt_name'], # 종목 이름
+                                 'hldg_qty': item['hldg_qty'], # 보유 수량
+                                 'ord_psbl_qty': item['ord_psbl_qty'], # 주문 가능 수량
+                                 'pchs_avg_pric': item['pchs_avg_pric'], # 매입 평균 가격
+                                 'prpr': item['prpr'], # 현재가
+                                 'evlu_amt': item['evlu_amt'], # 평가금액
+                                 'evlu_pfls_amt': item['evlu_pfls_amt'], # 수익
+                                 'evlu_pfls_rt': item['evlu_pfls_rt'] # 수익률
+                                 }
+        
+        print(f"종목번호: {item['pdno']}, 종목이름: {item['prdt_name']}, 보유수량:{item['hldg_qty']}, 주문가능수량:{item['ord_psbl_qty']}, 매입평균가격:{item['pchs_avg_pric']}, 현재가:{item['prpr']}, 평가금액:{item['evlu_amt']}, 수익:{item['evlu_pfls_amt']}, 수익률:{item['evlu_pfls_rt']}%")
+    
+    for item in current_account:
+        Current_account = {'dnca_tot_amt': item['dnca_tot_amt'], # 예수금 총 금액
+                        'nxdy_excc_amt':item['nxdy_excc_amt'], # 익일 정산 금액
+                        'prvs_rcdl_excc_amt':item['prvs_rcdl_excc_amt'], # 가수도 정산 금액
+                        'thdt_buy_amt':item['thdt_buy_amt'], # 금일 매수 금액
+                        'thdt_sll_amt':item['thdt_sll_amt'], # 금일 매도 금액
+                        'tot_evlu_amt':item['tot_evlu_amt'], # 총 평가 금액
+                        'nass_amt':item['nass_amt'], # 순 자산 금액
+                        'asst_icdc_amt':item['asst_icdc_amt'], # 자산 증감액
+                        'asst_icdc_erng_rt':item['asst_icdc_erng_rt'], # 자산 증감 수익률
+                        }
+        
+        print(f"예수금총금액:{item['dnca_tot_amt']}, 금일매수금액:{item['thdt_buy_amt']}, 금일매도금액:{item['thdt_sll_amt']}, 총평가금액:{item['tot_evlu_amt']}, 자산증감액:{item['asst_icdc_amt']}, 자산증감수익률:{item['asst_icdc_erng_rt']}%")
+    
+    time.sleep(0.5)
+    
+    return Current_stock, Current_account
 
 # 주식 매수 가능 여부 조회
 def CheckBuyStock(stock, target_buy_price):
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from function import *
 
+Open_price, Target_buy_price = OpenPrice(Stock_list, Open_price, Target_buy_price, Current_stock)
+
+CheckStock()
+
 while(True):
-    
-    Open_price, Target_buy_price = OpenPrice(Stock_list, Open_price, Target_buy_price)
 
-    LivePrice(Stock_list, Target_buy_price, Actual_buy_price, Target_sell_price, Actual_sell_price)
+    LivePrice(Stock_list, Target_buy_price, Target_sell_price, Current_stock)
 
-    print(f'Open_price:{Open_price}')
-    print(f'Target_buy_price:{Target_buy_price}')
-    print(f'Target_sell_price:{Target_sell_price}')
-    print(f'Actual_buy_price:{Actual_buy_price}')
-    print(f'Actual_sell_price:{Actual_sell_price}')
-    print(f'Quantity:{Quantity}')
-    print(f'Log:{Log}')
